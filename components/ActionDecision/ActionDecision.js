9bf04743def6976df931398d3985033cb0647c06
LosEagle <6146756+LosEagle@users.noreply.github.com>
2021-06-23 21:11:32 +0200

    feat: add ActionInput component

--- components/ActionDecision/ActionDecision.js
+++ components/ActionDecision/ActionDecision.js
@@ -1,16 +1,26 @@
 const React = require('react');
 const { Box, Text, Newline } = require('ink');
+const importJsx = require('import-jsx');
+
+const ActionInput = importJsx('../ActionInput/ActionInput');
 
 const ActionDecision = () => (
-  <Box>
-    <Text>
-      <Text>1) Attack</Text>
-      <Newline />
-      <Text>2) Use health potion</Text>
-      <Newline />
-      <Text>3) Rest</Text>
-    </Text>
-  </Box>
+  <>
+    <Box flexDirection="column">
+      <Box>
+        <Text>
+          <Text>1) Attack</Text>
+          <Newline />
+          <Text>2) Use health potion</Text>
+          <Newline />
+          <Text>3) Rest</Text>
+        </Text>
+      </Box>
+      <Box marginTop="1">
+        <ActionInput />
+      </Box>
+    </Box>
+  </>
 );
 
 module.exports = ActionDecision;
---
+++ components/ActionInput/ActionInput.js
@@ -0,0 +1,10 @@
+const React = require('react');
+const { Text } = require('ink');
+
+const ActionDecision = () => {
+  return (
+    <Text>$</Text>
+  );
+};
+
+module.exports = ActionDecision;
